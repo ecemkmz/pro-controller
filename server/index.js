c7350398ce75daedcf6949818cff9880e7bde329
azraagull <azragul85@gmail.com>
2023-12-28 01:30:22 +0300

    feat:Client and server install

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
